cf89d4854326dd0c09f296f53dfd9a08f2b2f3a5
Vincent Biret <vibiret@microsoft.com>
2021-03-15 16:23:22 -0400

    - adds missing non-null annotations

--- src/main/java/com/microsoft/graph/models/GroupPolicyUploadedDefinitionFileUploadNewVersionParameterSet.java
+++ src/main/java/com/microsoft/graph/models/GroupPolicyUploadedDefinitionFileUploadNewVersionParameterSet.java
@@ -22,15 +22,16 @@ import java.util.ArrayList;
 public class GroupPolicyUploadedDefinitionFileUploadNewVersionParameterSet {
     /**
      * The content.
-     * 
+     *
      */
     @SerializedName(value = "content", alternate = {"Content"})
     @Expose
+    @Nonnull
     public byte[] content;
 
     /**
      * The group Policy Uploaded Language Files.
-     * 
+     *
      */
     @SerializedName(value = "groupPolicyUploadedLanguageFiles", alternate = {"GroupPolicyUploadedLanguageFiles"})
     @Expose
