77daa27a076b2213b814079ba84625d3085025a7
raghucha <raghucha.078@gmail.com>
2025-06-05 15:22:40 -0400

    Added new constructor for GraphServiceClient
    fixes #2354
    
    Fixes [#]2354(#2354)
    
    -Allows Us to Overide the default Base URL in use cases such as GCC customers

--- src/main/java/com/microsoft/graph/beta/serviceclient/GraphServiceClient.java
+++ src/main/java/com/microsoft/graph/beta/serviceclient/GraphServiceClient.java
@@ -65,6 +65,16 @@ public class GraphServiceClient extends com.microsoft.graph.beta.BaseGraphServic
     public GraphServiceClient(@Nonnull OkHttpClient client) {
         this(new AnonymousAuthenticationProvider(), client);
     }
+     /**
+     * Instantiate the GraphServiceClient using an AuthenticationProvider, baseUrl and OkHttpClient.
+     * @param authenticationProvider The AuthenticationProvider for this GraphServiceClient.
+     * @param client The OkHttpClient for the GraphServiceClient.
+     * @param clouds The Clouds for the GraphServiceClient.
+     *
+     */
+    public GraphServiceClient(@Nonnull AuthenticationProvider authenticationProvider, @Nonnull OkHttpClient client, @Nonnull Clouds clouds) {
+        this(new BaseGraphRequestAdapter(authenticationProvider, clouds, "v1.0", getGraphClientOptions()));
+    }
     /**
      * Instantiate the GraphServiceClient using a TokenCredential and Scopes.
      * @param tokenCredential The TokenCredential for this GraphServiceClient.
